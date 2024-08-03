76dbf67561e609cc0ea786daa69383c4ea666c55
B0lleken <5579636+B0lleken@users.noreply.github.com>
2024-08-03 23:59:12 +0200

    Refactor createServer()

--- app.mjs
+++ app.mjs
@@ -2,17 +2,31 @@
 import { createServer } from 'node:http';
 import * as url from 'node:url';
 import * as path from 'node:path';
-import { createReadStream, exists, statSync, readFile } from 'node:fs';
+import { statSync, readFile } from 'node:fs';
 
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME || '127.0.0.1';
 const publicFolder = process.env.PUBLICFOLDER || './public';
 
+// maps the file extensions to MIME types
+const map = {
+	'.ico': 'image/x-icon',
+	'.html': 'text/html',
+	'.js': 'text/javascript',
+	'.json': 'application/json',
+	'.css': 'text/css',
+	'.png': 'image/png',
+	'.jpg': 'image/jpeg',
+	'.wav': 'audio/wav',
+	'.mp3': 'audio/mpeg',
+	'.svg': 'image/svg+xml',
+	'.pdf': 'application/pdf',
+	'.doc': 'application/msword'
+};
+
 // starts a http server locally on port 3000
-createServer((req, res) => {
-  //res.writeHead(200, { 'Content-Type': 'text/html' });
-  //createReadStream('public/index.html').pipe(res);
-  
+const app = createServer((req, res) => {
+
   console.log(`${req.method} ${req.url}`);
 
   // parse URL
@@ -22,51 +36,60 @@ createServer((req, res) => {
   let pathname = `${publicFolder}${originalPathname}`;
   // based on the URL path, extract the file extension. e.g. .js, .doc, ...
   let ext = path.parse(pathname).ext;
-  // maps file extension to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
 
-  exists(pathname, function (exist) {
-    if (!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 404;
-      res.end(`File ${originalPathname} not found!`);
+  // By adding "throwIfNoEntry: false", we prevent the synchronous stats retrieval of the path
+  // from throwing an exception and cause it to instead return "undefined" when there is no
+  // filesystem entry, e.g. the path doesn't exist.
+  const pathStats = statSync(pathname, { throwIfNoEntry: false });
+
+  // check for existing path (file or directory)
+  if (typeof pathStats === 'undefined') {
+	  // do API logic for request
       return;
-    }
+  }
 
-    // if is a directory search for index file matching the extension
-    if (statSync(pathname).isDirectory()) {
-		ext = ext || '.html';
-		pathname += '/index' + ext;
-	}
+  // check whether the path is a directory
+  if (pathStats.isDirectory() === true) {
+	  // check for a GET request
+	  if (req.method !== 'GET') {
+		  // do API logic for non-GET requests
+		  return;
+	  }
 
-    // read file from file system
-    readFile(pathname, function(err, data){
-      if (err) {
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader( 'Content-type', map[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
+	  // When it is a GET request
+	  // then search for index file matching the extension
+	  const indexFileStats = statSync(pathname + '/index.html', { throwIfNoEntry: false });
+
+	  // check for an index file
+	  if (typeof indexFileStats === 'undefined') {
+		  // do API logic for GET requests
+		  return;
+	  }
+
+	  // When an index file exists, then update the ext & pathname
+	  ext = '.html';
+	  pathname += '/index' + ext;
+  }
+
+  // read the file from file system
+  readFile(pathname, function(err, data){
+	  if (err) {
+		res.statusCode = 500;
+		res.end(`Error getting the file: ${err}.`);
+		return;
+	  }
+
+	  // when the file could be read without issues then set Content-type and send data
+	  res.setHeader( 'Content-type', map[ext] || 'text/plain' );
+	  res.end(data);
   });
-  
-}).listen(port, hostname, () => {
+
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server listening on port ${hostname}:${port}`);
 });
+
+
 
 // run with `node app.mjs`
